e69e73e355eb60d7e5a1ce34cce74effab12bc8e
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-04-07 03:27:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -47,10 +47,10 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel  @New_movies_hindi_eng</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", " FILE : <code>{file_name}</code> \n Size : <i>{file_size}</i> \n {previouscaption} @New_movies_hindi_eng")
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", " FILE : <code>{filename}</code> \n {previouscaption} @New_movies_hindi_eng")
 
 
 
